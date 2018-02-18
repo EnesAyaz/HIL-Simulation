7bfa350be69bcbfa8fa08cad85de886972377e77
DESKTOP-LQST6SJ\enes_ <enes.ayaz.1996@gmail.com>
2018-02-18 16:07:02 +0300

    Blink led at arduino by using matlab arduino board

---
+++ Blink_Matlab.m
@@ -0,0 +1,28 @@
+a=arduino; %define arduino boarda
+pin='D3'; %define pin number of arduino 
+HIGH=1; % define digital output 1
+LOW=0; % define digital output 0
+
+while (1)% it is required for infinite loop
+    
+    writeDigitalPin(a,pin,HIGH); % give pin a high voltage
+    for i=0:1:100000000
+    
+    
+    end 
+    % it provides me delay time for seeing leds open-closed loop
+    
+    
+    writeDigitalPin(a,pin,LOW); % give pin a low voltage
+    for i=0:1:100000000
+    
+    
+    end
+    % it provides me delay time
+    
+end
+
+% to start this function, it is needed to clear workspace,if
+% arduino is defined and still at tworkspace.
+
+
